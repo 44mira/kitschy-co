b03945d4e63e404cdf159199ceec8c65e10107a3
44mira <legolaslada@gmail.com>
2024-11-11 22:39:33 +0800

    feat: add routes for schema and swagger ui

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("docs/", include("kitschy_api.urls")),
+    path("docs/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("docs/", SpectacularSwaggerView.as_view(url_name="schema")),
 ]
