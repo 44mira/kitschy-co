178bd24c61637d31ca489df33b06117efc0d40dc
44mira <legolaslada@gmail.com>
2024-11-11 22:32:28 +0800

    fix: temporarily remove kitschy_api url reference

--- backend/kitschy_api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from rest_framework_swagger.views import get_swagger_view
-
-schema_view = get_swagger_view(title="Pastebin API")
-
-urlpatterns = [path(r"", schema_view)]
