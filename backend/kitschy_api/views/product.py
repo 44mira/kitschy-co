db5b545254e93c4379325779e01a9d4a20f17bd1
44mira <legolaslada@gmail.com>
2024-11-12 17:43:41 +0800

    feat: add product viewset

---
+++ backend/kitschy_api/views/product.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets
+from kitschy_api.models import Product
+from kitschy_api.serializers.product_serializer import ProductSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
