6399a42bacdc2571206bd42d164a5b3ec1d07bad
44mira <legolaslada@gmail.com>
2024-11-12 17:46:09 +0800

    feat: add product route

--- backend/kitschy_api/urls.py
+++ backend/kitschy_api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from kitschy_api.views.user import UserViewSet
+from kitschy_api.views.product import ProductViewSet
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet)
+router.register(r"products", ProductViewSet)
 urlpatterns = [
     path("api/", include(router.urls)),
 ]
-
