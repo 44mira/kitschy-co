3765f0aa17d5bc65612765175c36a9190ce0e08b
44mira <legolaslada@gmail.com>
2024-11-13 21:34:47 +0800

    feat: add total key to serializer

--- backend/kitschy_api/serializers/order_serializer.py
+++ backend/kitschy_api/serializers/order_serializer.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 
-from kitschy_api.models import Order
+from kitschy_api.models import Order, OrderItems
+from kitschy_api.serializers import OrderItemSerializer
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    total = serializers.SerializerMethodField()
+
     class Meta:
         model = Order
         fields = "__all__"
+
+    def get_total(self, obj):
+        items = OrderItems.objects.filter(order_id=obj.order_id)
+        if not items:
+            return 0
+
+        instance = OrderItemSerializer(items, many=True)
+
+        return sum(item["subtotal"] for item in instance.data)
