ad79935fa970828301102756e7703af12c06b37c
44mira <legolaslada@gmail.com>
2024-11-13 21:51:01 +0800

    feat: items key for orders

--- backend/kitschy_api/serializers/order_serializer.py
+++ backend/kitschy_api/serializers/order_serializer.py
@@ -6,16 +6,28 @@ from kitschy_api.serializers import OrderItemSerializer
 
 class OrderSerializer(serializers.ModelSerializer):
     total = serializers.SerializerMethodField()
+    items = serializers.SerializerMethodField()
 
     class Meta:
         model = Order
         fields = "__all__"
 
     def get_total(self, obj):
-        items = OrderItems.objects.filter(order_id=obj.order_id)
+        items = self.get_items(obj)
+
+        # check for empty
         if not items:
             return 0
 
+        return sum(item["subtotal"] for item in items)
+
+    def get_items(self, obj):
+        items = OrderItems.objects.filter(order_id=obj.order_id)
+
+        # check for empty
+        if not items:
+            return []
+
         instance = OrderItemSerializer(items, many=True)
 
-        return sum(item["subtotal"] for item in instance.data)
+        return instance.data
