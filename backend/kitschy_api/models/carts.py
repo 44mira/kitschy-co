11f8b2ec13f329676417e7aaf6b28a448c60835d
ewandeyb <aaronsiapuatco@gmail.com>
2024-11-13 14:50:44 +0800

    feat: added serializer for cart

--- backend/kitschy_api/models/carts.py
+++ backend/kitschy_api/models/carts.py
@@ -1,13 +1,20 @@
-from django.db import models
 import uuid
+
+from django.db import models
+
 from .users import User
 
-class Cart(models.Model):
-  cart_id = models.UUIDField(primary_key=True, default= uuid.uuid4, editable=False)
-  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="carts")
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
 
-  class Meta:
-    db_table = "carts"
-    ordering = ["-created_at"]
+class Cart(models.Model):
+    cart_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
+    )
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="carts"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "carts"
+        ordering = ["-created_at"]
