5a51423383202a202a0b67383254094e46ef860c
44mira <legolaslada@gmail.com>
2024-11-13 18:44:49 +0800

    format: pre-commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-f$jcdfi62_z8c3*05pm#^m%ufq7vm$qps^jy*t8xryhcuk0uog"
+SECRET_KEY = (
+    "django-insecure-f$jcdfi62_z8c3*05pm#^m%ufq7vm$qps^jy*t8xryhcuk0uog"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
