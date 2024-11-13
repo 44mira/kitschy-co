b54e88d1f67a1deca4ceb68d6fd98c8f9bc09c78
ewandeyb <aaronsiapuatco@gmail.com>
2024-11-13 14:13:26 +0800

    fix: added serializer and view for product

--- backend/kitschy_api/views/__init__.py
+++ backend/kitschy_api/views/__init__.py
@@ -1,5 +1,6 @@
 from .address import AddressViewSet
 from .order import OrderViewSet
 from .order_item import OrderItemList, OrderItemViewSet
+from .product import ProductViewSet
 from .product_image import ProductImageList, ProductImageViewSet
 from .user import UserViewSet
