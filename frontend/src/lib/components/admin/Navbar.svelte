95927c54a334bae172022b6ed467ae2d80d12f9c
Sharmaigne Angelie Mabano <sharmaignemabano@gmail.com>
2024-11-13 14:55:38 +0800

    refactor: use $lib instead of @

--- frontend/src/lib/components/admin/Navbar.svelte
+++ frontend/src/lib/components/admin/Navbar.svelte
@@ -1,11 +1,13 @@
 <script>
-	import { Button } from '@/components/ui/button';
-	import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-	import { Popover, PopoverTrigger, PopoverContent } from '@/components/ui/popover';
-	import logo from '@/assets/admin/logo.png';
+	import {Button} from '$lib/components/ui/button';
+	import { Avatar, AvatarFallback, AvatarImage } from '$lib/components/ui/avatar';
+	import { Popover, PopoverTrigger, PopoverContent } from '$lib/components/ui/popover';
+	import logo from '$lib/assets/admin/logo.png';
 </script>
 
-<nav class="h-fit flex items-center bg-brand-base border border-b-[3px] border-b-brand-purple-d p-2 px-10 justify-between sticky">
+<nav
+	class="h-fit flex items-center bg-brand-base border border-b-[3px] border-b-brand-purple-d p-2 px-10 justify-between sticky"
+>
 	<div class="h-full">
 		<a href="/" target="_blank"><img src={logo} alt="kitschy co." width="200" /></a>
 	</div>
