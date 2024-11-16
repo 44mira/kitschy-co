615374bb58f709eca3525b02ae677fc7c2e2d35b
ewandeyb <aaronsiapuatco@gmail.com>
2024-11-16 15:03:25 +0800

    feat: removed unnecessary price in cart_item and added appropriate total and subtotal methods

--- backend/kitschy_api/models/cart_items.py
+++ backend/kitschy_api/models/cart_items.py
@@ -16,7 +16,7 @@ class CartItem(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name="cart_items"
     )
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
 
     class Meta:
         db_table = "cart_items"
--- backend/kitschy_api/serializers/cart_item_serializer.py
+++ backend/kitschy_api/serializers/cart_item_serializer.py
@@ -4,6 +4,12 @@ from kitschy_api.models import CartItem
 
 
 class CartItemSerializer(serializers.ModelSerializer):
+    
+    subtotal = serializers.SerializerMethodField()
     class Meta:
         model = CartItem
         fields = "__all__"
+    
+    
+    def get_subtotal(self,obj)->float:
+        return obj.product.price * obj.quantity
