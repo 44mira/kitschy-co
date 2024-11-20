58605c6ac61b4596baa782460ae4e6ed03d07ddb
Sharmaigne Angelie Mabano <sharmaignemabano@gmail.com>
2024-11-20 12:33:58 +0800

    feat: add addProduct schema

---
+++ frontend/src/routes/admin/schema.ts
@@ -0,0 +1,12 @@
+import {z} from 'zod';
+
+export const addProductSchema = z.object({
+  name: z.string().min(1),
+  productImages: z.array(z.string()).min(1),
+  category: z.string().min(1),
+  creators: z.array(z.string()).min(1),
+  price: z.number().min(0),
+  description: z.string().min(1)
+});
+
+export type AddProductSchema = typeof addProductSchema;
