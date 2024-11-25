29983fe92c074e5c4663473e71596a6a0d25f743
44mira <legolaslada@gmail.com>
2024-11-25 23:31:43 +0800

    feat: use query params instead of a separate action

--- backend/kitschy_api/views/product.py
+++ backend/kitschy_api/views/product.py
@@ -1,7 +1,5 @@
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
 
 from kitschy_api.models import Product
 from kitschy_api.serializers.product_serializer import ProductSerializer
@@ -13,13 +11,4 @@ from kitschy_api.serializers.product_serializer import ProductSerializer
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-    @action(detail=False)
-    def ready(self):
-        """
-        Retrieves the list of **READY** products
-        """
-        ready_products = Product.objects.filter(status="READY")
-
-        serializer = self.get_serializer(ready_products, many=True)
-        return Response(serializer.data)
+    filterset_fields = ["status"]
