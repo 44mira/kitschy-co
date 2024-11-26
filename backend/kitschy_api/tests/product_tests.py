9087863c18284674fb4f4503f031c44ec47419f0
ewandeyb <aaronsiapuatco@gmail.com>
2024-11-26 09:52:01 +0800

    feat: completed basic tests for CRUD

--- backend/kitschy_api/tests/product_tests.py
+++ backend/kitschy_api/tests/product_tests.py
@@ -17,12 +17,13 @@ class TestProductAPI:
     def test_get_all_products(self, api_client):
         """GET api/products/ - get all products"""
 
-        baker.make("Product", _quantity=10) #Creates 10 instances of Product, each with random data
+        baker.make(
+            "Product", _quantity=10
+        )  # Creates 10 instances of Product, each with random data
         response = api_client.get("/api/products/")
 
         assert response.status_code == 200
         assert len(response.data) == 10
-        
 
     def test_get_single_product(self, api_client):
         product = baker.make("Product")
@@ -41,12 +42,23 @@ class TestProductAPI:
 
         assert response.status_code == 201
         assert response.data["name"] == product["name"]
-        """
+        
+    def test_update_product(self, authenticated_user):
+       product = baker.make("Product")
+       response = authenticated_user.put(f"/api/products/{product.product_id}/", {
+            "name": "Updated Product",
+            "desc": "Updated Description",
+            "price": 20.00,
+            "quantity": 20,
+           })
 
-    def test_update_product():
-        pass
+       assert response.status_code == 200
+    
+    def test_delete_product(self, authenticated_user):
+        product = baker.make("Product")
+        response = authenticated_user.delete(f"/api/products/{product.product_id}/")
 
+        assert response.status_code == 204
+        assert not response.data
+        
 
-    def test_delete_product():
-        pass
- """
