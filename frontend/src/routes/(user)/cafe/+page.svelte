ad967f5f2e2178493016122c51ca27fbc791f8db
44mira <legolaslada@gmail.com>
2024-12-11 22:44:13 +0800

    feat: add CafeItem

--- frontend/src/routes/(user)/cafe/+page.svelte
+++ frontend/src/routes/(user)/cafe/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import roof from '$lib/assets/users/roof.png';
 	import Hero from './Hero.svelte';
+	import CafeItem from './CafeItem.svelte';
 	import mockProducts from './mockData';
 </script>
 
@@ -12,18 +13,22 @@
 			alt="cafe and pastries roof"
 		/>
 	</div>
+
 	<Hero />
+
 	<div class="px-6 bg-inherit">
-		<div class="min-w-full bg-brand-base min-h-52 grow"></div>
+		<div class="min-w-full px-5 py-24 bg-brand-base min-h-52 grow -z-10">
+			<div class="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10">
+				{#each mockProducts as mockProduct}
+					<CafeItem {...mockProduct} />
+				{/each}
+			</div>
+		</div>
 	</div>
 </div>
 
 <style>
 	.cafe__body {
 		background-color: #ffe3cc;
-	}
-
-	.cafe__door {
-		background-color: #949494;
 	}
 </style>
---
+++ frontend/src/routes/(user)/cafe/CafeItem.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { type ProductSchema } from '@/api/schema';
+	import plate from '$lib/assets/users/plate.png';
+
+	import Button from '@/lib/components/ui/button/button.svelte';
+
+	type ProductSchemaProps = ProductSchema & { children?: import('svelte').Snippet };
+
+	let product: ProductSchemaProps = $props();
+</script>
+
+<div class="flex flex-col items-center justify-center min-h-fit min-w-fit">
+	<div class="relative z-0">
+		<img src={plate} alt="plate backdrop" class="z-0" />
+		<!-- product.images[0] goes here at a higher z -->
+	</div>
+
+	<div class="bg-white min-w-full min-h-40 max-h-52 p-4 -mt-12 z-[2]">
+		<h1 class="item__desc text-2xl font-bold">{product.name}</h1>
+		<p class="item__desc text-lg px-1">{product.desc}</p>
+
+		<div class="flex pt-5">
+			<Button
+				class="text-5xl content-center text-secondary-foreground border-secondary-foreground"
+				variant="outline"
+				size="icon">+</Button
+			>
+			<div class="grow"></div>
+			<div class="text-brand-orangePastries font-giphurs text-3xl font-bold">P{product.price}</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	.item__desc {
+		@apply font-giphursSC text-[#762804];
+	}
+</style>
