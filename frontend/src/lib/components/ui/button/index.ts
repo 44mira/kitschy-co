89cbb362e1105d70ee11e7de7a1649c21c308301
44mira <legolaslada@gmail.com>
2024-12-12 01:13:27 +0800

    feat: checkout segment

--- frontend/src/lib/components/ui/button/index.ts
+++ frontend/src/lib/components/ui/button/index.ts
@@ -13,7 +13,8 @@ const buttonVariants = tv({
 			ghost: 'hover:bg-accent hover:text-accent-foreground',
 			link: 'text-primary underline-offset-4 hover:underline',
 			tab: 'bg-background rounded-b-none',
-			cafe: 'rounded-none bg-brand-base text-[#BF3A0E] font-giphursSC font-bold text-2xl drop-shadow-xl'
+			cafe: 'rounded-none bg-brand-base text-[#BF3A0E] font-giphursSC font-bold text-2xl drop-shadow-xl',
+			big: 'font-gladiola bg-brand-purple hover:bg-brand-purple-m active:bg-brand-purple-d text-6xl'
 		},
 		size: {
 			default: 'h-10 px-4 py-2',
--- frontend/src/routes/(user)/cart/+page.svelte
+++ frontend/src/routes/(user)/cart/+page.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
+	import Checkout from './Checkout.svelte';
 </script>
 
+<div class="bg-brand-base max-h-fill min-h-[91vh] flex flex-col p-36 py-24">
+	<div class="grid grid-cols-3 gap-3 grow">
+		<div class="segment col-span-2"></div>
+		<div class="segment min-w-96">
+			<Checkout />
+		</div>
+	</div>
+</div>
+
 <style>
+	.segment {
+		border: 2px solid black;
+		border-radius: 2em;
+	}
 </style>
