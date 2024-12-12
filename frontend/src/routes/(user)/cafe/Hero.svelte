65a0ed59b20b45306dd92b3ee0719adf48a420b9
Kevlar277 <rkabrasada@up.edu.ph>
2024-12-12 20:48:02 +0800

    chore: adjusted door responsiveness

--- frontend/src/routes/(user)/cafe/Hero.svelte
+++ frontend/src/routes/(user)/cafe/Hero.svelte
@@ -7,10 +7,10 @@
 	<div class="px-12 sm:px-12 md:px-24 lg:px-24 xl:px-32 flex flex-col md:flex-row justify-between items-center gap-10 pt-4 pb-12 sm:pb-16 md:pb-16 lg:pb-20 -mt-10 sm:-mt-16 md:-mt-24 lg:-mt-32 relative">
 		<!-- Text Section -->
 		<div class="flex flex-col gap-6 md:gap-10 mt-16 sm:mt-24 md:mt-32 lg:mt-40">
-			<h1 class="text-7xl sm:text-8xl md:text-6xl lg:text-7xl xl:text-8xl text-brand-orangePastries font-gladiola font-bold drop-shadow-lg text-center md:text-left">
+			<h1 class="text-7xl sm:text-7xl md:text-5xl lg:text-6xl xl:text-7xl text-brand-orangePastries font-gladiola font-bold drop-shadow-lg text-center md:text-left">
 				Cafe & Pastries
 			</h1>
-			<div class="text-[#5C1700] font-giphurs text-2xl sm:text-2xl md:text-xl lg:text-2xl leading-relaxed max-w-full md:max-w-[500px] text-justify">
+			<div class="text-[#5C1700] font-giphurs text-2xl sm:text-2xl md:text-xl lg:text-xl xl:text-2xl leading-relaxed max-w-full md:max-w-[500px] text-justify">
 				This is a pretty short call to action text.
 				<span class="text-[#BF3A0E]">I am very excited</span>
 				and I want you to know too!
@@ -23,7 +23,7 @@
 
 		<!-- Cafe Door -->
 		<div
-			class="cafe__door rounded rounded-t-[999px] h-[300px] sm:h-[400px] md:h-[400px] lg:h-[450px] w-[250px] sm:w-[300px] md:w-[400px] lg:w-[450px] self-center md:self-end -mb-[50px] sm:-mb-[75px] md:-mb-[100px]"
+			class="cafe__door rounded rounded-t-[999px] h-[300px] sm:h-[400px] md:h-[325px] lg:h-[325px] xl:h-[350px] w-[300px] sm:w-[400px] md:w-[600px] lg:w-[700px] self-center md:self-end -mb-[50px] sm:-mb-[75px] md:-mb-[100px]"
 		></div>
 	</div>
 </div>
