4e02928f289ce6d064a689b52b1180f45511a697
44mira <legolaslada@gmail.com>
2024-12-13 23:06:30 +0800

    feat: add coming soon page

--- frontend/src/routes/(user)/+layout.svelte
+++ frontend/src/routes/(user)/+layout.svelte
@@ -8,7 +8,7 @@
 	let { children }: Props = $props();
 </script>
 
-<div class="flex flex-col min-h-screen">
+<div class="flex flex-col min-h-screen max-h-screen">
 	<Navbar />
 	{@render children?.()}
 </div>
