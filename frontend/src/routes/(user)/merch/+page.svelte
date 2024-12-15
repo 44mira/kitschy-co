f6ca24848c51c263cdfd9f688e2de8de55c9d480
44mira <legolaslada@gmail.com>
2024-12-16 04:30:50 +0800

    format: +page.svelte
    format

--- frontend/src/routes/(user)/merch/+page.svelte
+++ frontend/src/routes/(user)/merch/+page.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import Carousel from './Carousel.svelte'; // Import the carousel component
-  </script>
-  
-  <div class="bg-brand-base max-h-fill min-h-[91vh]">
+</script>
+
+<div class="bg-brand-base max-h-fill min-h-[91vh]">
 	<!-- Carousel Section -->
 	<Carousel />
-  </div>
-  
+</div>
