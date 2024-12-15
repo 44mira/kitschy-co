3aaafa47bc54b8f80c9a6b57cf68abe206f1fb2d
ewandeyb <aaronsiapuatco@gmail.com>
2024-12-15 20:57:44 +0800

    feat: displayed nested images path in products

--- backend/kitschy_api/models/product_images.py
+++ backend/kitschy_api/models/product_images.py
@@ -13,7 +13,7 @@ class ProductImage(models.Model):
         primary_key=True, default=uuid.uuid4, editable=False
     )
     product = models.ForeignKey(
-        Product, on_delete=models.CASCADE, related_name="products"
+        Product, on_delete=models.CASCADE, related_name="images"
     )
     img_url = models.FileField(upload_to=get_s3_path, null=True, blank=False)
     alt_desc = models.TextField()
--- backend/kitschy_api/serializers/product_image_serializer.py
+++ backend/kitschy_api/serializers/product_image_serializer.py
@@ -6,4 +6,4 @@ from kitschy_api.models import ProductImage
 class ProductImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductImage
-        fields = "__all__"
+        fields = ['product_image_id', 'img_url', 'alt_desc']
