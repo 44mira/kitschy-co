ddafa061ad2cdf1de770944a48f9812ce656d296
Kevlar277 <rkabrasada@up.edu.ph>
2024-12-16 01:28:42 +0800

    feat: responsiveness on homepage hero

--- frontend/src/routes/(user)/home/(hero)/CallToAction.svelte
+++ frontend/src/routes/(user)/home/(hero)/CallToAction.svelte
@@ -9,13 +9,21 @@
 	const { src, secondary } = $props();
 </script>
 
-<div class="grow-0 basis-[30%]">
-	<p class="mb-5 font-galdeano">
+<div class="grow-0 basis-full sm:basis-[30%] px-4 sm:px-6 lg:px-8">
+	<!-- Responsive Text -->
+	<p class="mb-5 font-galdeano text-base sm:text-base lg:text-lg xl:text-xl text-center sm:text-center md:text-left">
 		At kitschy co. ✨, our goal is to create a unique experience 🌟 and a welcoming environment 🏡
 		that will entice our customers' to return time and time again 💫.
 	</p>
-	<div class="flex h-52">
-		<img style:scale="0.40" class="head--tilt2" {src} alt="girl tilting her head" />
+
+	<!-- Responsive Image and Signup Dialog -->
+	<div class="flex sm:flex-row items-center justify-center md:justify-start h-auto">
+		<img
+			class="head--tilt2 w-28 sm:w-32 md:w-36 lg:w-40 object-contain"
+			style:scale="0.40"
+			{src}
+			alt="girl tilting her head"
+		/>
 		<SignupDialog data={data.signupForm} primary={secondary} />
 	</div>
 </div>
--- frontend/src/routes/(user)/home/(hero)/HeroMessage.svelte
+++ frontend/src/routes/(user)/home/(hero)/HeroMessage.svelte
@@ -2,15 +2,39 @@
 	const { src, secondary } = $props();
 </script>
 
-<div class="flex flex-col grow-0 basis-[30%]" style="--outline: black">
-	<div class="font-allura text-7xl">
-		where <span class="outlined text-5xl font-gasoekOne" style:color={secondary}>charm</span>
+<div
+	class="flex flex-col grow-0 basis-[30%] px-4 sm:px-6 lg:px-8 space-y-4 sm:space-y-6"
+	style="--outline: black"
+>
+	<!-- Responsive Header -->
+	<div class="font-allura text-5xl sm:text-3xl xl:text-6xl text-center sm:text-center md:text-left">
+		where
+		<span
+			class="outlined text-3xl sm:text-4xl lg:text-5xl font-gasoekOne"
+			style:color={secondary}
+		>
+			charm
+		</span>
 		meets
-		<span class="outlined text-5xl font-gasoekOne" style:color={secondary}>convenience</span>
+		<span
+			class="outlined text-3xl sm:text-4xl lg:text-5xl font-gasoekOne"
+			style:color={secondary}
+		>
+			convenience
+		</span>
 	</div>
-	<div class="flex items-center">
-		<img class="head--tilt" style:scale="0.40" {src} alt="girl tilting her head" />
-		<p class="-mx-10 font-galdeano">Since 2019, kitschy co. has been slaying!</p>
+
+	<!-- Image on the Left, Text on the Right -->
+	<div class="flex flex-row items-center justify-center md:justify-start">
+		<img
+			class="head--tilt w-28 sm:w-32 md:w-36 lg:w-40 object-contain"
+			style:scale="0.40"
+			{src}
+			alt="girl tilting her head"
+		/>
+		<p class="text-base sm:text-lg lg:text-xl text-left font-galdeano">
+			Since 2019, kitschy co. has been slaying!
+		</p>
 	</div>
 </div>
 
