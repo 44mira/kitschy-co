d8f966c4f4846e5d4d588763fbf3afd5fc689b70
ewandeyb <aaronsiapuatco@gmail.com>
2024-12-17 11:59:17 +0800

    format:format

--- backend/kitschy_api/serializers/product_serializer.py
+++ backend/kitschy_api/serializers/product_serializer.py
@@ -9,13 +9,24 @@ class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
-        fields = ["name", "desc", "price", "quantity", "status", "category", "images"]
+        fields = [
+            "name",
+            "desc",
+            "price",
+            "quantity",
+            "status",
+            "category",
+            "images",
+        ]
+
     def create(self, validated_data):
-        images_data = validated_data.pop('images', [])  # Remove images data and default to empty list
+        images_data = validated_data.pop(
+            "images", []
+        )  # Remove images data and default to empty list
         product = Product.objects.create(**validated_data)
-        
+
         # Handle any provided images
         for image_data in images_data:
             ProductImage.objects.create(product=product, **image_data)
-            
-        return product  
+
+        return product
